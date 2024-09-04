f2c86c484a8107fe49d5f13a622c78952ed998a1
nivemula <nitinchandravemula.com>
2024-09-03 23:19:21 -0400

    routing

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,15 +2,14 @@ import React from 'react';
 import styled from 'styled-components';
 
 const DashboardContainer = styled.div`
-  /* Add your styling here */
   padding: 20px;
 `;
 
 function Dashboard() {
   return (
     <DashboardContainer>
-      <h1>Dashboard</h1>
-      {/* Add your dashboard content here */}
+      <h2>Dashboard</h2>
+
     </DashboardContainer>
   );
 }
